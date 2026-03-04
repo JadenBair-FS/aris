7397e8e7ea73d3615531ff6a9fc4c1a2c3b33334
JadenBair <jmbair@student.fullsail.edu>
2026-03-04 11:06:02 -0700

    back to mistral

--- backend/src/ARIS.API/Program.cs
+++ backend/src/ARIS.API/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddSingleton<IEmbeddingGenerator<string, Embedding<float>>>(sp 
 
 builder.Services.AddSingleton<IChatClient>(sp =>
 {
-    var client = new OllamaApiClient(ollamaUri, "qwen3.5:4b");
+    var client = new OllamaApiClient(ollamaUri, "mistral");
     client.SetTimeout(TimeSpan.FromHours(1));
     return client;
 });
